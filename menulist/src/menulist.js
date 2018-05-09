c92761d6d0c0dda2cab052742adaec4244c07c3a
wanchai <>
2018-05-09 10:20:23 +0700

    menulist

---
+++ menulist/src/menulist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class menulist extends Component {
+    state={
+        menu:''
+    }
+
+    render() {
+        return (
+            <div>
+                <button></button>
+            </div>
+        );
+    }
+}
+
+export default menulist;
